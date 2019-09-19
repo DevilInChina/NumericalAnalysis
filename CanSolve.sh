7248f797903abad42921e91c87e6c5fc92357c59
DevilInChina <905320627@qq.com>
2019-09-19 14:20:37 +0800

    A script marks for can solved function to matrix

---
+++ CanSolve.sh
@@ -0,0 +1,14 @@
+./main doo 3matrices/Ac.txt
+./main cro 3matrices/Ac.txt
+./main gs 3matrices/Ac.txt 
+./main gs 3matrices/Am.txt 
+./main sor 3matrices/Ac.txt
+./main sor 3matrices/Am.txt
+./main cg 3matrices/Af.txt
+./main doo newmtx/Am_new.txt
+./main cro newmtx/Am_new.txt 
+./main gs newmtx/Am_new.txt 
+./main sor newmtx/Am_new.txt 
+./main cg newmtx/Am_new.txt 
+
+
